a25685c4faf18886b2cace803a3f8ec82c9eb63b
Léon Blacquiere <leonblacquiere@Mac.domain_not_set.invalid>
2025-07-11 09:48:03 +0200

    Fix: remove deprecated serveAdmin from medusa-config

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,7 +5,6 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    serveAdmin: false,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
